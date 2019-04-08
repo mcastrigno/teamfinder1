d7078d4c1c191a92cb2ae445ec22c4e03c5ab546
Russell Leininger <russellleininger@u.boisestate.edu>
2019-04-08 11:26:34 -0600

    Made it mobile first

--- src/main/resources/static/js/group.js
+++ src/main/resources/static/js/group.js
@@ -3,43 +3,65 @@ const getUserInfo = function() {
         type: "GET",
         url: "/user/info",
     }).then(function(response) {
-        console.log("response: ",response);
         let header = document.getElementById("welcome-user");
         header.innerText = "Welcome, " + response;
     });
 };
 
-const writeAvailTable = function(num) {
-    let table = '';
-    let rows = 8;
-    let cols = 8;
-    for (let r = 0; r < rows; r++) {
-        table += '<tr>';
-        for (let c = 0; c < cols; c++) {
-            if (c == 0) {
-                let time = r+8;
-                if (time == 12) {
-                    time = "Noon"
-                } else {
-                    time = time > 12 ? (time%12) + " pm": time + " am";
-                }
-                table += '<td class="dCell">' + time + '</td>';
+const makeTables = function() {
+    let groups = ["group1","group2","group3"];
+    let headers = ["Time","Sun","Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
+    let table;
+    let td;
+    let row;
+
+    groups.forEach(function(group) {
+        table = document.getElementById(group);
+        let th = document.createElement("tr");
+        for(let i = 0; i < 8; i++) {
+            td = document.createElement("td");
+            td.innerText = headers[i];
+            if (i == 0) {
+                td.classList.add("dCell");
             } else {
-                let id = (r*8)+c;
-                let color;
-                if (num==1) {
-                    color = (c%2) && (r>1 && r<4) ? "green": "red";
-                } else if (num==2) {
-                    color = ((c%2==0) && (r%4<2)) ? "green": "red";
-                } else if (num==3) {
-                    color = "red";
-                }
-                table += '<td id="'+id+'" class="aCell '+color+'"></td>';
+                td.classList.add("aCell");
             }
+            th.appendChild(td);
         }
-        table += '</tr>';
-    }
-    document.write('<table class="table table-bordered">' + table + '</table>');
+        table.appendChild(th);
+        let rows = 8;
+        let cols = 8;
+        for (let r = 0; r < rows; r++) {
+            row = document.createElement("tr");
+            for (let c = 0; c < cols; c++) {
+                td = document.createElement("td");
+                if (c == 0) {
+                    let time = r+8;
+                    if (time == 12) {
+                        time = "Noon"
+                    } else {
+                        time = time > 12 ? (time%12) + "pm": time + "am";
+                    }
+                    td.innerText = time;
+                    td.classList.add("dCell");
+                } else {
+                    let id = (r*8)+c;
+                    let color;
+                    if (group=='group1') {
+                        color = (c%2) && (r>1 && r<4) ? "green": "red";
+                    } else if (group=='group2') {
+                        color = ((c%2==0) && (r%4<2)) ? "green": "red";
+                    } else if (group=='group3') {
+                        color = "red";
+                    }
+                    td.classList.add("aCell",color);
+                    td.id = id;
+                }
+                row.appendChild(td);
+            }
+            table.appendChild(row);
+        }
+    })
 };
 
 const switchTable = function() {
--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -1,27 +1,48 @@
-const writeAvailTable = function() {
-   let table = '';
-   let rows = 8;
-   let cols = 8;
-   for (let r = 0; r < rows; r++) {
-       table += '<tr>';
-       for (let c = 0; c < cols; c++) {
-           if (c == 0) {
-               let time = r+8;
-               if (time == 12) {
-                   time = "Noon"
-               } else {
-                   time = time > 12 ? (time%12) + " pm": time + " am";
-               }
-               table += '<td class="dCell">' + time + '</td>';
-           } else {
-               let id = (r*8)+c;
-               let color = ((c%2==0) && (r%4<2)) || ((c%2) && (r>1 && r<5)) ? "green": "red";
-               table += '<td id="'+id+'" class="aCell '+color+'" onclick="toggleColor('+id+')"></td>';
-           }
-       }
-       table += '</tr>';
-   }
-   document.write('<table class="table table-bordered">' + table + '</table>');
+const buildTable = function() {
+    let headers = ["Time","Sun","Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
+    let table;
+    let td;
+    let row;
+
+    table = document.getElementById("table");
+    let th = document.createElement("tr");
+    for(let i = 0; i < 8; i++) {
+        td = document.createElement("td");
+        td.innerText = headers[i];
+        if (i == 0) {
+            td.classList.add("dCell");
+        } else {
+            td.classList.add("aCell");
+        }
+        th.appendChild(td);
+    }
+    table.appendChild(th);
+    let rows = 8;
+    let cols = 8;
+    for (let r = 0; r < rows; r++) {
+        row = document.createElement("tr");
+        for (let c = 0; c < cols; c++) {
+            td = document.createElement("td");
+            if (c == 0) {
+                let time = r+8;
+                if (time == 12) {
+                    time = "Noon"
+                } else {
+                    time = time > 12 ? (time%12) + "pm": time + "am";
+                }
+                td.innerText = time;
+                td.classList.add("dCell");
+            } else {
+                let id = (r*8)+c;
+                let color = ((c%2==0) && (r%4<2)) || ((c%2) && (r>1 && r<5)) ? "green": "red";
+                td.classList.add("aCell",color);
+                td.id = id;
+                td.onclick = function() {toggleColor(id);}
+            }
+            row.appendChild(td);
+        }
+        table.appendChild(row);
+    }
 };
 
 const toggleColor = function(id) {
