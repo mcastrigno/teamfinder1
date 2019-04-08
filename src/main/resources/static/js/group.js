d8a449ebd9e6332743b30bf8156289cf8ace9e9c
Russell Leininger <russellleininger@u.boisestate.edu>
2019-04-08 00:15:14 -0600

    Made colored tables

--- src/main/resources/static/js/group.js
+++ src/main/resources/static/js/group.js
@@ -9,7 +9,7 @@ const getUserInfo = function() {
     });
 };
 
-const writeAvailTable = function() {
+const writeAvailTable = function(num) {
     let table = '';
     let rows = 8;
     let cols = 8;
@@ -17,12 +17,41 @@ const writeAvailTable = function() {
         table += '<tr>';
         for (let c = 0; c < cols; c++) {
             if (c == 0) {
-                table += '<td class="dCell">' + r + '</td>';
+                let time = r+8;
+                if (time == 12) {
+                    time = "Noon"
+                } else {
+                    time = time > 12 ? (time%12) + " pm": time + " am";
+                }
+                table += '<td class="dCell">' + time + '</td>';
             } else {
-                table += '<td class="aCell">' + c + '</td>';
+                let id = (r*8)+c;
+                let color;
+                if (num==1) {
+                    color = (c%2) && (r>1 && r<4) ? "green": "red";
+                } else if (num==2) {
+                    color = ((c%2==0) && (r%4<2)) ? "green": "red";
+                } else if (num==3) {
+                    color = "red";
+                }
+                table += '<td id="'+id+'" class="aCell '+color+'"></td>';
             }
         }
         table += '</tr>';
     }
     document.write('<table class="table table-bordered">' + table + '</table>');
+};
+
+const switchTable = function() {
+    let show = document.getElementById("group-select").value;
+    let groups = ["group1","group2","group3"];
+    let table;
+    groups.forEach(function(group) {
+        table = document.getElementById(group);
+        if (group == show) {
+            table.style.display = "block";
+        } else {
+            table.style.display = "none";
+        }
+    })
 };
--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -6,12 +6,29 @@ const writeAvailTable = function() {
        table += '<tr>';
        for (let c = 0; c < cols; c++) {
            if (c == 0) {
-               table += '<td class="dCell">' + r + '</td>';
+               let time = r+8;
+               if (time == 12) {
+                   time = "Noon"
+               } else {
+                   time = time > 12 ? (time%12) + " pm": time + " am";
+               }
+               table += '<td class="dCell">' + time + '</td>';
            } else {
-               table += '<td class="aCell">' + c + '</td>';
+               let id = (r*8)+c;
+               let color = ((c%2==0) && (r%4<2)) || ((c%2) && (r>1 && r<5)) ? "green": "red";
+               table += '<td id="'+id+'" class="aCell '+color+'" onclick="toggleColor('+id+')"></td>';
            }
        }
        table += '</tr>';
    }
    document.write('<table class="table table-bordered">' + table + '</table>');
+};
+
+const toggleColor = function(id) {
+    let td = document.getElementById(id);
+    if (td.classList.contains("green")) {
+        td.classList.replace("green", "red");
+    } else {
+        td.classList.replace("red", "green");
+    }
 };
